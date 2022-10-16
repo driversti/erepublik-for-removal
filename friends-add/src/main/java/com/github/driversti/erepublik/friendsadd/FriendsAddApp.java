15ce033a5d0e9b285a7a18358b27801d2b96ef2a
driversti <github@seniorjava.dev>
2022-10-16 14:56:03 +0200

    added console logger (friends-add)

--- friends-add/src/main/java/com/github/driversti/erepublik/friendsadd/FriendsAddApp.java
+++ friends-add/src/main/java/com/github/driversti/erepublik/friendsadd/FriendsAddApp.java
@@ -1,8 +1,13 @@
 package com.github.driversti.erepublik.friendsadd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FriendsAddApp {
 
+  protected static final Logger log = LogManager.getLogger(FriendsAddApp.class);
+
   public static void main(String[] args) {
-    System.out.println("Works!");
+    log.info("Works!");
   }
 }
